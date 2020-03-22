68d9d806ec929f4222ca7577858bb9138fc02cb9
kingdaro <kingdaro@gmail.com>
2020-03-21 23:23:10 -0500

    it works i guess

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ import {
 } from "typescript-template-language-service-decorator"
 import ts from "typescript/lib/tsserverlibrary"
 
-function createLanguageService(): TemplateLanguageService {
+function createLanguageService(
+  info: ts.server.PluginCreateInfo,
+): TemplateLanguageService {
   return {
     getCompletionsAtPosition(context, position) {
-      const line = context.text.split(/\n/g)[position.line]
       return {
         isGlobalCompletion: false,
         isMemberCompletion: false,
         isNewIdentifierLocation: false,
         entries: [
           {
-            name: line.slice(0, position.character),
-            kind: ts.ScriptElementKind.label,
-            kindModifiers: "echo",
-            sortText: "echo",
+            name: "awesomeness",
+            kind: ts.ScriptElementKind.string,
+            sortText: "awesomeness",
           },
         ],
       }
@@ -32,7 +32,7 @@ export = function init(mod: { typescript: typeof ts }) {
         mod.typescript,
         info.languageService,
         info.project,
-        createLanguageService(),
+        createLanguageService(info),
         { tags: ["tw"] },
       )
     },
