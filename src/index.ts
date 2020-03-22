0ed72e053d47b13ed7e999b0213c4ccc88a25ed6
kingdaro <kingdaro@gmail.com>
2020-03-22 16:11:32 -0500

    some progress

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import path from "path"
+// @ts-ignore
+import corePlugins from "tailwindcss/lib/corePlugins"
+// @ts-ignore
+import processPlugins from "tailwindcss/lib/util/processPlugins"
+// @ts-ignore
+import resolveTailwindConfig from "tailwindcss/lib/util/resolveConfig"
+// @ts-ignore
+import defaultTailwindConfig from "tailwindcss/stubs/defaultConfig.stub"
 import {
   decorateWithTemplateLanguageService,
   TemplateLanguageService,
@@ -7,8 +16,37 @@ import ts from "typescript/lib/tsserverlibrary"
 function createLanguageService(
   info: ts.server.PluginCreateInfo,
 ): TemplateLanguageService {
+  const log = (...stuff: unknown[]) => {
+    const output = stuff
+      .map((value) => JSON.stringify(value, null, 2))
+      .join(" ")
+
+    return info.project.projectService.logger.info(
+      `[twin-macro-autocomplete] ${output}`,
+    )
+  }
+
   return {
     getCompletionsAtPosition(context, position) {
+      // log("getting completions")
+
+      const configPath = path.resolve(
+        info.project.getCurrentDirectory(),
+        `./tailwind.config.js`,
+      )
+      log(configPath)
+
+      const config = resolveTailwindConfig([
+        require(configPath),
+        defaultTailwindConfig,
+      ])
+
+      const processedPlugins = processPlugins(
+        [...corePlugins(config), ...config.plugins],
+        config,
+      )
+      log(processedPlugins)
+
       return {
         isGlobalCompletion: false,
         isMemberCompletion: false,
